4ea339473ab9f6a9c176202933b6b9866b9e3d55
sami el <s.elidrissidafali@elaraki.ac.ma>
2024-09-17 09:07:27 -0400

    ajout models users

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,29 +1,28 @@
-const nodemailer=require("nodemailer");
+const nodemailer = require("nodemailer");
 
-module.exports=async(email,subject,text)=>{
-    try {
-        const transporter=nodemailer.createTransport({
-            host:process.env.HOST,
-            service:process.env.SERVICE,
-            post:Number(process.env.EMAIL_PORT),
-            secure:Boolean(process.env.SECURE),
-            auth:{
-                user:process.env.USER,
-                pass:process.env.PASS
-            }
+module.exports = async (email, subject, text) => {
+	try {
+		const transporter = nodemailer.createTransport({
+			host: process.env.HOST,
+			service: process.env.SERVICE,
+			port: Number(process.env.EMAIL_PORT),
+			secure: Boolean(process.env.SECURE),
+			auth: {
+				user: process.env.USER,
+				pass: process.env.PASS,
+			},
+		});
 
-        });
-        await transporter.sendMail({
-            from:process.env.USER,
-            to:email,
-            subject:subject,
-            text:text
-        })
-        console.log("Email sent succesfully")
-    } 
-   
-    catch (error) {
-       console.log ("Email not sent");
-       console.log(error);
-    }
-}
+		await transporter.sendMail({
+			from: process.env.USER,
+			to: email,
+			subject: subject,
+			text: text,
+		});
+		console.log("email sent successfully");
+	} catch (error) {
+		console.log("email not sent!");
+		console.log(error);
+		return error;
+	}
+};
